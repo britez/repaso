33b7a60013c9a55752d2a7ba1bd206d1e99bf234
Andrea Urdininea <aurdininea@redb.ee>
2022-12-16 09:04:46 -0300

    revision

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,16 +3,138 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		sum(1,2);
-	}
+    public static void main(String[] args) {
 
-	public static void sum(Integer a, Integer b) {
-		System.out.println("Resultado: " + (a + b));
-	}
+        List<Integer> numbersArray = Arrays.asList(1, 4, 1);
+        int[] list = {8, 7, 9, 4, 5, 3, 6};
+        int[] numbers = {7, 13, 20, 5, 9, 32, 10, 6};
+
+        sum(2, 2, 3);
+        sumarLista(numbersArray);
+        largerNumbers(numbers);
+        maximumNumber(3, 4, 1);
+        ascendingList(list);
+        descendingList(list);
+
+    }
+
+    public static int sum(Integer a, Integer b, Integer c) {            // ejercicio 1
+        Integer result = a + b + c;
+        System.out.println("- La suma de los numeros ingresados es: " + result);
+        return result;
+    }
+
+    public static int sumarLista(List<Integer> numbersArray) {           //ejercicio 2
+
+        int suma = 0;
+        for (Integer e : numbersArray) {
+            suma = suma + e;
+
+        }
+        System.out.println("- La suma de la lista es: " + suma);
+        return suma;
+    }
+
+    public static ArrayList<Integer> largerNumbers(int[] numbers) {        // ejercicio 3
+
+
+        ArrayList<Integer> numbersGreaterThanTen = new ArrayList<Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > 10) {
+                numbersGreaterThanTen.add(numbers[i]);
+            }
+        }
+        System.out.println("- Los numeros mayores a diez son: " +numbersGreaterThanTen);
+        return numbersGreaterThanTen;
+    }
+
+    public static int maximumNumber(Integer a, Integer b, Integer c) {      //ejercicio 4
+        Integer max;
+        if (a > b) {
+            if (a > c) {
+                max = a;
+            } else {
+                max = c;
+            }
+        } else if (b > c) {
+            max = b;
+        } else {
+            max = c;
+        }
+
+        System.out.println("- El maximo entre los tres numeros es: " + max);
+        return max;
+
+    }
+    public static int[] ascendingList(int [] list) {          //ejercicio 5
+
+        for (int j = 0; j < list.length; j++) {
+
+            for (int i = 0; i < (list.length - 1 -j); i++) {
+                if (list[i] > list[i + 1]) {
+                    int aux = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = aux;
+                }
+            }
+        }
+        //cuando j= 0 e i = 0 queda 7,8,9,4,5,3,6
+        //cuando j= 0 e i = 0 queda 7,8,9,4,5,3,6
+        //cuando i= 2 queda 7,8,4,9,5,3,6
+        //cuando i= 3 queda 7,8,4,5,9,3,6
+        //cuando i= 4 queda 7,8,4,5,3,9,6
+        //cuando i= 5 queda 7,8,4,5,3,6,9
+
+
+        //cuando i=0 queda 7,8,4,5,3,6,9
+        //cuando i=1 queda 7,4,8,5,3,6,9
+        //cuando i=2 queda 7,4,5,8,3,6,9
+        //cuando i=3 queda 7,4,5,3,8,6,9
+        //cuando i=4 queda 7,4,5,3,6,8,9
+
+        //cuando i=0 queda 4,7,5,3,6,8,9
+        //cuando i=1 queda 4,5,7,3,6,8,9
+        //cuando i=2 queda 4,5,3,7,6,8,9
+        //cuando i=3 queda 4,5,3,6,7,8,9
+
+        //cuando i=0 queda 4,5,3,6,7,8,9
+        //cuando i=1 queda 4,3,5,6,7,8,9
+        //cuando i=2 queda 4,3,5,6,7,8,9
+
+        //cuando i=0 queda 3,4,5,6,7,8,9
+        //cuando i=1 queda 3,4,5,6,7,8,9
+
+
+
+
+        System.out.println("- La lista actualizada de forma ascendiente es: " + Arrays.toString(list));
+        return list;
+    }
+
+
+    public static int[] descendingList(int[] list) {          //ejercicio 6
+
+
+        for (int j = 0; j < list.length; j++) {
+
+            for (int i = 0; i < (list.length - j -1); i++) {
+                if (list[i] < list[i + 1]) {
+                    int aux = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = aux;
+                }
+            }
+        }
+        System.out.println("- La lista actualizada de forma descendiente es: " + Arrays.toString(list));
+        return list;
+    }
+
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,127 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class DemoApplicationTests {
+//suma de 3 numeros
+	@Test
+	public Integer testSum() {
+		Integer result = DemoApplication.sum(2,3,5);
+		assertEquals(10, result);
+		return result;
+	}
 
 	@Test
-	void contextLoads() {
+	@DisplayName("Test when null values are sent")
+	public void testSumNull() {
+		Integer result2 = DemoApplication.sum(null, 3, 5);
+		assertNotNull(result2, "The result cannot be null");
+		assertEquals(8, result2);
 	}
+
+	@Test
+	@DisplayName("Test when null values are sent")
+	public void testSumNull2() {
+		Integer result2 = DemoApplication.sum(2, null, 5);
+		assertNotNull(result2, "The result cannot be null");
+		assertEquals(7, result2);
+	}
+
+	@Test
+	@DisplayName("Test when null values are sent")
+	public void testSumNull3() {
+		Integer result3 = DemoApplication.sum(4, 1, null);
+		assertNotNull(result3, "The result cannot be null");
+		assertEquals(5, result3);
+	}
+	// sumar lista
+	@Test
+	public void testSumLista() {
+		int result4 = DemoApplication.sumarLista(Arrays.asList(1, 4, 1));
+		assertEquals(12, result4);
+		//return result4;
+	}
+
+	//mayores a 10
+
+	@Test
+	@DisplayName("Test when null values are sent")
+	public void testMayores() {
+		int[] numbers = {7, 13, 20, 5, 9, 32, 10, 6};
+		int[] numbersLargerTen = {13, 20, 5, 32, 10};
+		List<Integer> resultMayores = DemoApplication.largerNumbers(numbers);
+		assertNotNull(resultMayores, "The result cannot be null");
+		assertEquals(numbersLargerTen, resultMayores);
+	}
+	//Maximo de los 3 numeros
+	// hacer para el caso que sea null de los 3 numeros
+	@Test
+	@DisplayName("Test when null values are sent")
+	public void testMax() {
+		Integer result5 = DemoApplication.maximumNumber(2, 8, 5);
+		assertNotNull(result5, "The result cannot be null");
+		assertEquals(8, result5);
+	}
+
+	@Test
+	@DisplayName("Test when null values are sent")
+	public void testMaxNull() {
+		Integer result6 = DemoApplication.maximumNumber(null, 8, 5);
+		assertNotNull(result6, "The result cannot be null");
+		assertEquals(8, result6);
+	}
+
+	@Test
+	@DisplayName("Test when null values are sent")
+	public void testMaxNull1() {
+		Integer result7 = DemoApplication.maximumNumber(2, 1, null);
+		assertNotNull(result7, "The result cannot be null");
+		assertEquals(2, result7);
+	}
+
+	@Test
+	@DisplayName("Test when null values are sent")
+	public void testAscendingList() {
+		int[] list = {8, 7, 9, 4, 5, 3, 6};
+		int[] listResult = {3, 4, 5, 6, 7, 8, 9};
+
+		int[] resultAscendingList = DemoApplication.ascendingList(list);
+		assertNotNull(resultAscendingList, "The result cannot be null");
+		assertEquals(listResult, resultAscendingList);
+	}
+
+	// en caso de que todos sean 0
+	@Test
+	@DisplayName("Test when null values are sent")
+	public void testAscendingListNull() {
+		int[] list = {0, 0, 0, 0, 0, 0, 0};
+		int[] listResult = {0, 0, 0, 0, 0, 0, 0};
+
+		int[] resultAscendingList = DemoApplication.ascendingList(list);
+		assertNotNull(resultAscendingList, "The result cannot be null");
+		assertArrayEquals(listResult, resultAscendingList);
+	}
+
+	@Test
+	@DisplayName("Test when null values are sent")
+	public void testADecendingList() {
+		int[] list = {8, 7, 9, 4, 5, 3, 6};
+		int[] listResult = {9, 8, 7, 6, 5, 4, 3};
+
+		int[] resultAscendingList = DemoApplication.descendingList(list);
+		assertNotNull(resultAscendingList, "The result cannot be null");
+		assertEquals(listResult, resultAscendingList);
+	}
+
+	//
+
 
 }
